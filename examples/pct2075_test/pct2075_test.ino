537aaff810a2b6c634539e235da3d3af8c240974
brentru <brent@adafruit.com>
2025-10-29 16:31:01 -0400

    Bump semver and fix

--- examples/pct2075_test/pct2075_test.ino
+++ examples/pct2075_test/pct2075_test.ino
@@ -6,16 +6,13 @@
 
 Adafruit_PCT2075 PCT2075;
 
-uint8_t i2c_addr = PCT2075_I2CADDR_DEFAULT;  // default address (see guide for others)
-
 void setup() {
-
   Serial.begin(115200);
   // Wait until serial port is opened
   while (!Serial) { delay(1); }
   Serial.println("Adafruit PCT2075 Test");
 
-  if (!PCT2075.begin(i2c_addr)) {
+  if (!PCT2075.begin(PCT2075_I2CADDR_DEFAULT)) {
     Serial.println("Couldn't find PCT2075 chip");
     while (1);
   }
